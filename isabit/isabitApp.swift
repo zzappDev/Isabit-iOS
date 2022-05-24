72d085dd9842e34df0bf9f0f61def9112004cd96
KJH <kjhg5852@naver.com>
2022-05-24 15:55:41 +0900

    Initial Commit

---
+++ isabit/isabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  isabitApp.swift
+//  isabit
+//
+//  Created by KJH on 2022/05/24.
+//
+
+import SwiftUI
+
+@main
+struct isabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
